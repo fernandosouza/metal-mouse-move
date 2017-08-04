5df0b82d2c453033a934895d64581fe3466945fb
Liferay <liferay@Liferays-MacBook-Pro.local>
2017-08-04 16:25:33 -0300

    Initial commit

---
+++ __tests__/MouseMove.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import MouseMove from '../src/MouseMove';
+
+describe('MouseMove', function() {
+	it('should be tested', function() {
+		expect('No tests for this module yet.').toBe('Everything is ok.');
+	});
+});
---
+++ src/MouseMove.js
@@ -0,0 +1,15 @@
+'use strict';
+
+import State from 'metal-state';
+
+class MouseMove extends State {
+    constructor() {
+        super();
+    }
+}
+
+MouseMove.STATE = {
+};
+
+export { MouseMove };
+export default MouseMove;
